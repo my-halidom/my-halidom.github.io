ffe591ac435a91c5b4115c427437c4dcd81e42a0
Drunken Debugger <drunkendebugger@gmail.com>
2019-02-22 12:00:30 -0700

    Updated to latest showcase: "Dragon Special"

--- scripts/showcase-data.js
+++ scripts/showcase-data.js
@@ -59,16 +59,30 @@
 // const featuredThreeStarsPercentage = (4.0 / 100.0) - ((fiveStarFudgeFactor / 2.0) * (4.0 / 8.0));
 // const nonFeaturedThreeStarsPercentage = (29.75 / 100.0) - ((fiveStarFudgeFactor / 2.0) * (29.75 / 72.0));
 
-const CURRENT_SHOWCASE_NAME = "Ardent Admirers";
-const limitedGachaInclusions = [Adventurer.VALENTINES_HILDEGARDE, Adventurer.VALENTINES_EZELITH, Adventurer.VALENTINES_ORION];
+// const CURRENT_SHOWCASE_NAME = "Ardent Admirers";
+// const limitedGachaInclusions = [Adventurer.VALENTINES_HILDEGARDE, Adventurer.VALENTINES_EZELITH, Adventurer.VALENTINES_ORION];
+// const gachaPool = NonLimitedGachaAdventurers.concat(limitedGachaInclusions);
+// const featuredItems = [Adventurer.VALENTINES_HILDEGARDE, Adventurer.VALENTINES_EZELITH, Adventurer.VALENTINES_ORION];
+
+// const fiveStarFudgeFactor = (0.04682263 - 0.04); // Experimentally determined. Accounts for the "pity" mechanic where five-star odds rise as successive non-five-stars are summoned.
+
+// const featuredFiveStarsPercentage = (0.5 / 100.0) + ((fiveStarFudgeFactor / 2.0) * (0.5 / 2.0));
+// const nonFeaturedFiveStarsPercentage = (0.5 / 100.0) + ((fiveStarFudgeFactor / 2.0) * (0.5 / 2.0));
+// const featuredFourStarsPercentage = (1.75 / 100.0);
+// const nonFeaturedFourStarsPercentage = (3.07 / 100.0);
+// const featuredThreeStarsPercentage = (4.0 / 100.0) - ((fiveStarFudgeFactor / 2.0) * (4.0 / 8.0));
+// const nonFeaturedThreeStarsPercentage = (29.75 / 100.0) - ((fiveStarFudgeFactor / 2.0) * (29.75 / 72.0));
+
+const CURRENT_SHOWCASE_NAME = "Dragon Special";
+const limitedGachaInclusions = [];
 const gachaPool = NonLimitedGachaAdventurers.concat(limitedGachaInclusions);
-const featuredItems = [Adventurer.VALENTINES_HILDEGARDE, Adventurer.VALENTINES_EZELITH, Adventurer.VALENTINES_ORION];
+const featuredItems = [Adventurer.MIKOTO, Adventurer.VANESSA];
 
 const fiveStarFudgeFactor = (0.04682263 - 0.04); // Experimentally determined. Accounts for the "pity" mechanic where five-star odds rise as successive non-five-stars are summoned.
 
-const featuredFiveStarsPercentage = (0.5 / 100.0) + ((fiveStarFudgeFactor / 2.0) * (0.5 / 2.0));
-const nonFeaturedFiveStarsPercentage = (0.5 / 100.0) + ((fiveStarFudgeFactor / 2.0) * (0.5 / 2.0));
-const featuredFourStarsPercentage = (1.75 / 100.0);
+const featuredFiveStarsPercentage = (0.4 / 100.0) + ((fiveStarFudgeFactor / 2.0) * (0.4 / 2.0));
+const nonFeaturedFiveStarsPercentage = (0.4 / 100.0) + ((fiveStarFudgeFactor / 2.0) * (0.4 / 2.0));
+const featuredFourStarsPercentage = (1.25 / 100.0);
 const nonFeaturedFourStarsPercentage = (3.07 / 100.0);
-const featuredThreeStarsPercentage = (4.0 / 100.0) - ((fiveStarFudgeFactor / 2.0) * (4.0 / 8.0));
-const nonFeaturedThreeStarsPercentage = (29.75 / 100.0) - ((fiveStarFudgeFactor / 2.0) * (29.75 / 72.0));
+const featuredThreeStarsPercentage = 0.0;
+const nonFeaturedThreeStarsPercentage = (33.75 / 100.0) - (fiveStarFudgeFactor * (33.75 / 80.0));
